afdd239cab17aa8940e2d1969451145aa987b762
Nandakumar S <nandakumars5352@gmail.com>
2025-10-15 16:57:30 +0530

    Time: 0 ms (100%), Space: 55.4 MB (78.57%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.js
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    for(let i=1;i<matrix.length;i++){
-        for(let j=1;j<matrix[i].length;j++){
-            if(matrix[i][j]!==matrix[i-1][j-1]){
+    for(let i=0;i<matrix.length-1;i++){
+        for(let j=0;j<matrix[i].length-1;j++){
+            if(matrix[i][j]!==matrix[i+1][j+1]){
                 return false
             }
         }
