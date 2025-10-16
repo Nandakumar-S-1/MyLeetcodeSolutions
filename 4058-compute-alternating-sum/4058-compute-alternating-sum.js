9b34dbed68481183ea6f9a606016e1222a475355
Nandakumar S <nandakumars5352@gmail.com>
2025-10-16 17:07:52 +0530

    Time: 1 ms (48.8%), Space: 54.9 MB (95.85%) - LeetHub

---
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var alternatingSum = function(nums) {
+    let odd=0
+    let even=0
+    for(let i=0;i<nums.length;i++){
+        if(i%2==0){
+            even+=nums[i]
+        }else{
+            odd-=nums[i]
+        }
+    }
+    return odd+even
+};
