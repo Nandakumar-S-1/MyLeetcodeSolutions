1c4d5c6f3b85e77b1755ed5d6c64b8fa08825a22
Nandakumar S <nandakumars5352@gmail.com>
2025-11-02 18:30:34 +0530

    Time: 0 ms (100%), Space: 54 MB (36.18%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.js
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.js
@@ -2,6 +2,11 @@
  * @param {number} n
  * @return {boolean}
  */
-var isStrictlyPalindromic = function(n) {
-    return false
+var isStrictlyPalindromic = function (n) {
+    for (let i = 2; i <= n - 2; i++) {
+        let val = n.toString(i)
+        let rev = val.split('').reverse().join('')
+        if (val !== rev) return false
+    }
+    return true
 };
