32ab4ad8321bd50cb3d2568e8579f258c5b418b9
Nandakumar S <nandakumars5352@gmail.com>
2025-11-05 09:27:05 +0530

    Time: 2 ms (86.08%), Space: 58.8 MB (16.14%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.js
+++ 4107-find-missing-elements/4107-find-missing-elements.js
@@ -2,16 +2,12 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findMissingElements = function(nums) {
-    let not=[]
-    let min=Math.min(...nums)
-    let max=Math.max(...nums)
-    for(let i=min;i<=max;i++){
-        not.push(i)
-    }
-    let res=[]
-    for(let i of not){
-        if(!nums.includes(i)){
+var findMissingElements = function (nums) {
+    let min = Math.min(...nums)
+    let max = Math.max(...nums)
+    let res = []
+    for (let i = min; i <= max; i++) {
+        if (!nums.includes(i)) {
             res.push(i)
         }
     }
