ff57b5b4568f805122560345fd5ae1076b91139b
Nandakumar S <nandakumars5352@gmail.com>
2025-11-06 09:04:43 +0530

    Time: 579 ms (7.98%), Space: 55.7 MB (61.35%) - LeetHub

---
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var gcdOfOddEvenSums = function(n) {
+    let ec=0
+    let oc=0
+    let esum=0
+    let osum=0
+    let i=1
+    while(ec<n || oc<n){
+        if(i%2===0){
+            ec++
+            esum+=i
+        }else{
+            oc++
+            osum+=i
+        }
+        i++
+    }
+    let res=1
+    for(let i=0;i<Math.max(esum,osum);i++){
+        if(esum%i===0&&osum%i===0){
+            res=i
+        }
+    }
+    return res
+};
