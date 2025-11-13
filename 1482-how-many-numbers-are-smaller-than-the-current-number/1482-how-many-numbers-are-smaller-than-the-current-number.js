35f095b3da2e0fe673da6db08f63b8eedd4f7965
Nandakumar S <nandakumars5352@gmail.com>
2025-11-13 16:02:35 +0530

    Time: 8 ms (52.62%), Space: 56.6 MB (79.52%) - LeetHub

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    let count=0
+    let res=[]
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums.length;j++){
+            if(nums[i]>nums[j]){
+                count++
+            }
+        }
+        res.push(count)
+        count=0
+    }
+    return res
+};
