32cc97c6864a494590c9af8eb87210a0dc8eee21
Nandakumar S <nandakumars5352@gmail.com>
2025-11-13 16:00:15 +0530

    Time: 11 ms (58.96%), Space: 63 MB (28.57%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function (nums) {
+    let set = new Set()
+    let dup = 0
+    let miss = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            dup = nums[i]
+        }
+        set.add(nums[i])
+    }
+    for (let i = 1; i <= nums.length; i++) {
+        if(!set.has(i)){
+            miss=i
+        }
+    }
+    return [dup,miss]
+};
