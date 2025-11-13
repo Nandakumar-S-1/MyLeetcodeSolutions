1ec158ebae840e64d3ac22630aa02d2545216f23
Nandakumar S <nandakumars5352@gmail.com>
2025-11-13 15:48:27 +0530

    Time: 58 ms (37.88%), Space: 57.2 MB (64.67%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.js
+++ 1580-shuffle-the-array/1580-shuffle-the-array.js
@@ -4,12 +4,9 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    let a=nums.slice(0,n)
-    let b=nums.slice(n)
     let res=[]
     for(let i=0;i<n;i++){
-        res.push(a[i])
-        res.push(b[i])
+        res.push(nums[i],nums[i+n])
     }
     return res
 };
