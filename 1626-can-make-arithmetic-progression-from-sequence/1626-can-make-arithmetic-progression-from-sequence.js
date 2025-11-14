10aae0b9b50c1318b63e11c4546092411ca5651a
Nandakumar S <nandakumars5352@gmail.com>
2025-11-14 09:50:32 +0530

    Time: 1 ms (75.23%), Space: 55.1 MB (80.73%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.js
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.js
@@ -5,7 +5,6 @@
 var canMakeArithmeticProgression = function(arr) {
     let sorted=arr.sort((a,b)=>a-b)
     let diff=sorted[1]-sorted[0]
-    console.log(diff,sorted)
     for(let i=1;i<sorted.length;i++){
         if(diff!==sorted[i]-sorted[i-1]){
             return false
