ffb56f11d7cf0b610783165c0ab1186228dfcf36
Nandakumar S <nandakumars5352@gmail.com>
2025-11-16 14:34:06 +0530

    Time: 3 ms (98%), Space: 57.5 MB (64%) - LeetHub

--- 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.js
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.js
@@ -4,10 +4,5 @@
  * @return {number[]}
  */
 var maxKDistinct = function (nums, k) {
-    let set = new Set(nums)
-    let res = []
-    for (let i of set) {
-        res.push(i)
-    }
-    return res.sort((a, b) => b - a).slice(0, k)
+    return [...new Set(nums)].sort((a, b) => b - a).slice(0, k)
 };
