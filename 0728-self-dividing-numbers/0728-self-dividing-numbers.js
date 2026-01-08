2ca4487daa1a37fe66132bec388cd5ae94dc0999
Nandakumar S <nandakumars5352@gmail.com>
2026-01-08 15:54:34 +0530

    Time: 8 ms (18.54%), Space: 56.1 MB (39.76%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.js
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -5,18 +5,16 @@
  */
 var selfDividingNumbers = function (left, right) {
     let res = []
-
     for (let i = left; i <= right; i++) {
         let arr = i.toString().split('')
-        let isTrue=true
+        let isTrue = true
         for (let j of arr) {
-            let val=Number(j)
-            if (val===0 || i % val !== 0) {
-                isTrue=false
+            if (Number(j) === 0 || i % Number(j) !== 0) {
+                isTrue = false
                 break
             }
         }
-        if(isTrue){
+        if (isTrue) {
             res.push(i)
         }
     }
