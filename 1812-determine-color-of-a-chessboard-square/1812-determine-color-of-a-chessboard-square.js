afc822504e0ecd54acf5e6e3ab56781bc5b799d3
Nandakumar S <nandakumars5352@gmail.com>
2026-01-28 20:26:10 +0530

    Time: 0 ms (100%), Space: 53.7 MB (52.08%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
@@ -3,5 +3,10 @@
  * @return {boolean}
  */
 var squareIsWhite = function(c) {
-    return (c.charCodeAt(0)+c.charCodeAt(1))%2
+    let map = {
+        'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8
+    }
+    let x = Number(c[1])
+    let y= map[c[0]]
+    return (x+y)%2!==0 ? true : false
 };
