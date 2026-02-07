44cbf5fc0b931a10282817543d2551e8b302db5c
Nandakumar S <nandakumars5352@gmail.com>
2026-02-07 21:59:15 +0530

    Time: 2 ms (68.18%), Space: 57.5 MB (77.27%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var resultArray = function(nums) {
+    let a1=[nums[0]]
+    let a2=[nums[1]]
+    for(let i=2;i<nums.length;i++){
+        if(a1[a1.length-1]>a2[a2.length-1]){
+            a1.push(nums[i])
+        }else{
+            a2.push(nums[i])
+        }
+    }
+    return [...a1,...a2]
+};
