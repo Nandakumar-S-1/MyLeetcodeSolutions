e4ed6184696a7739e109882668c8df4ee61676bb
Nandakumar S <nandakumars5352@gmail.com>
2026-02-18 14:56:26 +0530

    Time: 4 ms (6.65%), Space: 54.2 MB (22.91%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {
+    let p = pattern.split('')
+    let w = s.split(' ')
+    if (p.length !== w.length) return false
+
+    let map = new Map()
+    let dup = new Map()
+    for (let i = 0; i < p.length; i++) {
+        if (!map.has(p[i])) {
+            if (dup.has(w[i])) {
+                return false
+            }
+            map.set(p[i],w[i])
+            dup.set(w[i],true)
+        } else {
+            if(map.get(p[i])!==w[i]){
+                return false
+            }
+        }
+    }
+    console.log(map,dup)
+    return true
+};
