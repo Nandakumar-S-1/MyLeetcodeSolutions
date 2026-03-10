2dfed2097361041b91fc760d48b73dcea3baf623
Nandakumar S <nandakumars5352@gmail.com>
2026-03-10 19:30:27 +0530

    Time: 0 ms (100%), Space: 53.1 MB (97.8%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function (word) {
+    if (word === word.toUpperCase() || word === word.toLowerCase()) return true;
+    else if (word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase()) return true;
+    else return false;
+};
