866606e015b1660798ee877a304f7f23637dd227
Nandakumar S <nandakumars5352@gmail.com>
2026-03-13 15:32:41 +0530

    Time: 52 ms (40.73%), Space: 56.1 MB (96.57%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.js
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -4,12 +4,10 @@
  */
 var validMountainArray = function (arr) {
     if (arr.length < 3) return false
-
     let i=0
     while(i<arr.length-1 && arr[i]<arr[i+1]){
         i++
     }
-    console.log(i,arr[i])
     if(i==0 || i==arr.length-1 || arr[i]===arr[arr.length-1]){
         return false
     }
@@ -17,8 +15,5 @@ var validMountainArray = function (arr) {
     while(i<arr.length-1 && arr[i]>arr[i+1]){
         i++
     }
-
     return i===arr.length-1 ? true : false
-
-    
 };
