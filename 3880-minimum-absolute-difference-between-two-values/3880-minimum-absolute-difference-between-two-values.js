0d9bb49f6b1c0dec697bf8f1c69d2b8bb201ed8c
Nandakumar S <nandakumars5352@gmail.com>
2026-04-04 19:38:07 +0530

    Time: 220 ms (4.63%), Space: 64.4 MB (5.69%) - LeetHub

---
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minAbsoluteDifference = function(nums) {
+    let res=Infinity
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums.length;j++){
+            if(nums[i]===1 &&nums[j]===2){
+                res = Math.min(res,Math.abs(i-j))
+                console.log(res,i,j)
+            }
+        }
+    }
+    return res===Infinity ? -1 : res
+};
