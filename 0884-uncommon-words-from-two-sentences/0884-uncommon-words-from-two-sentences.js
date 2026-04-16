15dbd2afc5c00a504fa4a24b1974f0a69eb5b015
Nandakumar S <nandakumars5352@gmail.com>
2026-04-16 11:43:18 +0530

    Time: 1 ms (80%), Space: 53.2 MB (88.4%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
+ */
+var uncommonFromSentences = function(s1, s2) {
+    let map={}
+    let arr=(s1+' '+s2).split(' ')
+    for(let i of arr){
+        map[i]=(map[i]||0)+1
+    }
+    let res=[]
+    for(let i in map){
+        if(map[i]===1){
+            res.push(i)
+        }
+    }
+    return res
+};
