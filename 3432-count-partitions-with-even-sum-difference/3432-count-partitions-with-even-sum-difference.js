216222b10cd54913b1dfda4a210ea184802144e1
Nandakumar S <nandakumars5352@gmail.com>
2026-04-17 18:43:13 +0530

    Time: 2 ms (25.97%), Space: 57.2 MB (27.92%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countPartitions = function(nums) {
+    let count=0
+    let left=[]
+
+    for(let i=0;i<nums.length-1;i++){
+        left.push(nums[i])
+        let right=[]
+        for(let j=i+1;j<nums.length;j++){
+            right.push(nums[j])    
+        }
+        let ls=left.reduce((acc,curr)=>acc+curr,0)
+        let rs=right.reduce((acc,curr)=>acc+curr,0)
+        if(Math.abs(ls-rs)%2===0){
+            count++
+        }
+    }
+    return count
+};
