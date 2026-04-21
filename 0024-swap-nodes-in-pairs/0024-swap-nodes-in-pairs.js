4d1c52fa6f1047ef31c68dbeb72b5b9de6139cd6
Nandakumar S <nandakumars5352@gmail.com>
2026-04-21 17:05:17 +0530

    Time: 0 ms (100%), Space: 54.1 MB (29.57%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head || !head.next){
+        return head
+    }
+    let fst=head
+    let sec=head.next
+
+    fst.next=swapPairs(sec.next)
+    sec.next=fst
+
+    return sec
+};
