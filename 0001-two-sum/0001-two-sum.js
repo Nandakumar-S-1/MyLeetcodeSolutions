4a16ab1afdf3209dff8eccb541e5fa6476d82ed0
Nandakumar S <nandakumars5352@gmail.com>
2026-05-03 23:06:52 +0530

    Time: 3 ms (55.34%), Space: 54.7 MB (61.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,15 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let i=0
-    let obj={}
-    while(i<nums.length){
-        let r = target-nums[i]
-        if(obj[r]!==undefined){
-            return [obj[r],i]
-        }
-        obj[nums[i]]=i
-        i++
+var twoSum = function (nums, target) {
+    let map = {}
+    let rem = 0
+    for (let i = 0; i < nums.length; i++) {
+        rem = target - nums[i]
+        if (rem in map) {
+            return [map[rem], i]
+        } map[nums[i]] = i
     }
+    return []
 };
