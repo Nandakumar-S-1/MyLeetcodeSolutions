b4eec07e38d1862346c796d8a37621e21e3f54e3
Nandakumar S <nandakumars5352@gmail.com>
2026-05-17 15:16:41 +0530

    Time: 2 ms (69.32%), Space: 58.3 MB (90.71%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -2,16 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function(nums) {
-    let count=0
-    let max=0
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===1){
+var findMaxConsecutiveOnes = function (nums) {
+    let count = 0
+    let total = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
             count++
-            max=Math.max(max,count)
-        }else{
-            count=0
+            if (total < count) {
+                total = count
+            }
+        } else {
+            count = 0
         }
     }
-    return max
+    return total
 };
