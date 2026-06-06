8127ca4ea658a06787bb824d348be576e45adab1
Nandakumar S <nandakumars5352@gmail.com>
2026-06-06 12:20:12 +0530

    Time: 66 ms (5.31%), Space: 59.8 MB (5.75%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -2,17 +2,35 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var leftRightDifference = function(nums) {
-    let n=nums.length
-    let l=0
-    let r=0
-    let res=[]
-    let sum=nums.reduce((a,c)=>a+c,0)
-    console.log(sum)
-    for(let i=0;i<nums.length;i++){
-        let r =sum-l-nums[i]
-        res[i]=Math.abs(l-r)
-        l+=nums[i]
+var leftRightDifference = function (nums) {
+    let n = nums.length
+    let l = []
+    let r = []
+    let i = 0
+    let res = []
+    while (i < n) {
+        if (i === 0) {
+            l[i] = 0
+        } else {
+            l[i] = nums.reduce((a, c, ind) => {
+                if (ind < i) {
+                    return a + c
+                }
+                return a
+            }, 0)
+        }
+        if (i === n-1) {
+            r[i] = 0
+        } else {
+            r[i] = nums.reduce((a, c, ind) => {
+                if (ind > i) {
+                    return a + c
+                }
+                return a
+            }, 0)
+        }
+        res[i]=Math.abs(l[i]-r[i])
+        i++
     }
     return res
 };
