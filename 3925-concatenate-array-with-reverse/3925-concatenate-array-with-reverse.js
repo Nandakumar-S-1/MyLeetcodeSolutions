3eb5887d686b2c8eb471ee9286ea788bb0996b3e
Nandakumar S <nandakumars5352@gmail.com>
2026-07-02 09:02:00 +0530

    Time: 1 ms (86.26%), Space: 60 MB (11.59%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
@@ -2,9 +2,9 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var concatWithReverse = function(nums) {
-    let n=nums.length
-    for(let i=n-1;i>=0;i--){
+var concatWithReverse = function (nums) {
+    let n = nums.length
+    for (let i = n - 1; i >= 0; i--) {
         nums.push(nums[i])
     }
     return nums
